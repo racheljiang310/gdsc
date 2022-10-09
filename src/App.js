69b7040c5b825f3e2fab9dd6f41a4bf25006a64d
Rachel <racheljiang310@gmail.com>
2022-10-09 07:45:27 -0700

    restart

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Routes, Route} from  'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
-      </Router>
-    
-    </>
-    
+    <p>Hello</p>
   );
 }
 export default App;
